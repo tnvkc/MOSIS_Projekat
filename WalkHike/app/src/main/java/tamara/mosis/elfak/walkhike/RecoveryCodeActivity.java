21f0244d23a295e402ad530da9d54d67815d2f20
Dragana <dragana.korunovic@gmail.com>
2020-05-15 10:56:21 +0200

    jos activity-ja

---
+++ WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/RecoveryCodeActivity.java
@@ -0,0 +1,14 @@
+package tamara.mosis.elfak.walkhike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecoveryCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recovery_code);
+    }
+}
