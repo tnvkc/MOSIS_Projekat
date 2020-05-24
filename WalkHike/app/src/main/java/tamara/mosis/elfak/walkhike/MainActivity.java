f128a4f42434299d3e182971c9e80e6c29886151
Tamara Sokolov <62897522+tamara-sokolov@users.noreply.github.com>
2020-05-24 19:50:29 +0200

    prikaz trenutne lokacije

--- WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/MainActivity.java
+++ WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/MainActivity.java
@@ -1,45 +1,77 @@
 package tamara.mosis.elfak.walkhike;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, OnMapReadyCallback {
+
+
 
     Button btn_notification;
+    private static final int PERMISSION_CODE = 1;
+    Location location;
+    LocationManager locationManager;
     Button btn_friendslist;
     Button btn_addobject;
     Button btn_completedroutes;
     Button btn_leaderboards;
     private GoogleMap map;
+    boolean isGpsProvider;
+    boolean isNetworkProvider;
+    GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_CODE);
+        } else {
+            map.setMyLocationEnabled(true);
+
+
+        }
+        getDeviceLocation();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.main_map_fragment);
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                    map = googleMap;
-                //LatLng latlng = new LatLng(28.1,  68.1);
-            }
-        });
+        mapFragment.getMapAsync(this);
 
 
         btn_addobject = findViewById(R.id.main_addObjects);
         btn_addobject.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(), AddNewObjectActivity.class);
+                Intent intent = new Intent(getApplicationContext(), AddNewObjectActivity.class);
                 startActivity(intent);
             }
         });
@@ -48,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         btn_friendslist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(), FriendslistActivity.class);
+                Intent intent = new Intent(getApplicationContext(), FriendslistActivity.class);
                 startActivity(intent);
             }
         });
@@ -57,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         btn_notification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(), NotificationsActivity.class);
+                Intent intent = new Intent(getApplicationContext(), NotificationsActivity.class);
                 startActivity(intent);
             }
         });
@@ -66,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
         btn_completedroutes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(), CompletedRoutesActivity.class);
+                Intent intent = new Intent(getApplicationContext(), CompletedRoutesActivity.class);
                 startActivity(intent);
             }
         });
@@ -75,10 +107,96 @@ public class MainActivity extends AppCompatActivity {
         btn_leaderboards.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(), LeaderboardsActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LeaderboardsActivity.class);
                 startActivity(intent);
             }
         });
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+
+//if you need to diable rotation
+        //googleMap.getUiSettings().setRotateGesturesEnabled(false);
+        //if you need to disable zooming
+        //googleMap.getUiSettings().setZoomGesturesEnabled(false);
+
+
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        this.map=googleMap;
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    map.setMyLocationEnabled(true);
+                } else {
+                    return;
+                }
+        }
+    }
+
+    private void getDeviceLocation() {
+        locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
+        isGpsProvider = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        isNetworkProvider = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        if (!isGpsProvider && !isNetworkProvider) {
+            //showing setting for enable gps
+            return;
+        } else {
+            GetLocationData();
+        }
+    }
+
+    private void GetLocationData() {
+        googleApiClient = new GoogleApiClient.Builder(MainActivity.this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(MainActivity.this)
+                .addOnConnectionFailedListener(MainActivity.this)
+                .build();
+
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        location=LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if(location!=null){
+
+            Toast.makeText(MainActivity.this, "Lat : "+location.getLatitude()+" Lng "+location.getLongitude(), Toast.LENGTH_SHORT).show();
+            if(map!=null){
+                LatLng latLng=new LatLng(location.getLatitude(),location.getLongitude());
+                map.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
+                map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10f));
+                Toast.makeText(this, "Usao u latlong", Toast.LENGTH_SHORT );
+            }
+        }
+        //startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
 
     }
 }
