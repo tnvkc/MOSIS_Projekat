2b2b29b75e03724652a1bebd419a046481f6fa7f
Tamara Sokolov <62897522+tamara-sokolov@users.noreply.github.com>
2020-05-28 18:19:12 +0200

    Dodate akcije za odlazak na settings, help itd iz profila

--- WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/ProfileActivity.java
+++ WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/ProfileActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -25,11 +29,38 @@ public class ProfileActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Profile");
 
         getSupportActionBar().setDisplayShowTitleEnabled(true);
-        list=(ListView) findViewById(R.id.listview_profile_options);
-        Context context=getApplicationContext();
-        CustomListView adapter=new CustomListView(this,context.getResources().getStringArray(R.array.profile_options),
-                imgid,false);
+        list = (ListView) findViewById(R.id.listview_profile_options);
+        Context context = getApplicationContext();
+        CustomListView adapter = new CustomListView(this, context.getResources().getStringArray(R.array.profile_options),
+                imgid, false);
         list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent;
+                switch (position) {
+                    case 0:
+                        intent = new Intent(getApplicationContext(), EditProfileActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        intent=new Intent(getApplicationContext(), HelpActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 3:
+                        //intent=new Intent(getApplicationContext(), EditProfileActivity.class);
+                        // startActivity(intent);
+                        Toast.makeText(getApplicationContext(), "LOGED OUT", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                }
+            }
+        });
 
 
     }
