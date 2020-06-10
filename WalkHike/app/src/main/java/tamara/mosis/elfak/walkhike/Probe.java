2e18e2a4ffda060a495cd5d4d0ff557b32c9fb0e
Tamara Sokolov <62897522+tamara-sokolov@users.noreply.github.com>
2020-06-10 21:28:58 +0200

    uradjen sign up

--- WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/Probe.java
+++ WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/Probe.java
@@ -3,9 +3,17 @@ package tamara.mosis.elfak.walkhike;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Probe extends AppCompatActivity {
+
+
+    Button signupB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,16 @@ public class Probe extends AppCompatActivity {
         else
             setTheme(R.style.AppThemeLight);
         setContentView(R.layout.activity_probe);
+
+
+
+        signupB = (Button) findViewById(R.id.proba_signup);
+        signupB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/SignUpActivity.java
+++ WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/SignUpActivity.java
@@ -1,11 +1,34 @@
 package tamara.mosis.elfak.walkhike;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    Button Signin;
+    EditText txtUsername;
+    EditText txtEmail;
+    EditText txtPassword;
+    private ProgressDialog progress;
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +39,56 @@ public class SignUpActivity extends AppCompatActivity {
             setTheme(R.style.AppThemeLight);
         setContentView(R.layout.activity_sign_up);
 
+       firebaseAuth = FirebaseAuth.getInstance();
+
+       progress = new ProgressDialog(this);
+
+        txtPassword = (EditText)   findViewById(R.id.signup_password);
+        txtEmail = (EditText)   findViewById(R.id.signup_email);
+
+        Signin = (Button) findViewById(R.id.signup_button_done);
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Toast.makeText(getApplicationContext(), "done", Toast.LENGTH_SHORT).show();
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser()
+    {
+        String email = txtEmail.getText().toString();
+        String pass = txtPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(getApplicationContext(), "empty email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(pass))
+        {
+            Toast.makeText(getApplicationContext(), "pass empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progress.setMessage("registering...");
+        progress.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(getApplicationContext(), "Signed in! " + txtEmail.getText() +  txtPassword.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "Could not register! ", Toast.LENGTH_SHORT).show();
+                    }
+                    progress.dismiss();
+            }
+        });
     }
 }
