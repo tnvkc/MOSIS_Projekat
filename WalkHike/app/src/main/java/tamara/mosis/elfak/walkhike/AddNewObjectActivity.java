912fdc02d13ab236d71ffa75bb985612a9ed119d
tnvkc <62897123+tnvkc@users.noreply.github.com>
2020-06-20 02:19:05 +0200

    pocetak za AddNewObjectActivity, layout-i za odgovarajuce fragmente

--- WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/AddNewObjectActivity.java
+++ WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/AddNewObjectActivity.java
@@ -1,11 +1,22 @@
 package tamara.mosis.elfak.walkhike;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
-public class AddNewObjectActivity extends AppCompatActivity {
+import tamara.mosis.elfak.walkhike.Fragments.AddCheckpoint_EditObject;
+import tamara.mosis.elfak.walkhike.Fragments.AddObject_SelectType;
+import tamara.mosis.elfak.walkhike.Fragments.AddObject_SetDetails;
+
+public class AddNewObjectActivity extends FragmentActivity implements View.OnClickListener {
+
+    private int state = 0;
+    Button btnPrev, btnNext;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +26,92 @@ public class AddNewObjectActivity extends AppCompatActivity {
         else
             setTheme(R.style.AppThemeLight);
         setContentView(R.layout.activity_add_new_object);
+
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setMax(300);
+
+        btnPrev = findViewById(R.id.add_object_prev);
+        btnNext = findViewById(R.id.add_object_next);
+
+        btnPrev.setOnClickListener(this);
+        btnNext.setOnClickListener(this);
+
+        btnPrev.setEnabled(false); //inicijalno
+
+        AddObject_SelectType firstFragment = new AddObject_SelectType();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, firstFragment).commit();
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_object_prev: {
+                //go to prev fragment
+                btnNext.setEnabled(true);
+                state--;
+                if (state == 0) {
+                    btnPrev.setEnabled(false);
+
+                    AddObject_SelectType firstFragment = new AddObject_SelectType();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, firstFragment)
+                            .addToBackStack(null).commit();
+
+                    progressBar.setProgress(0);
+                }
+                else if (state == 1) {
+                    AddCheckpoint_EditObject secondFragment = new AddCheckpoint_EditObject();
+                    //bundle with arguments...
+
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, secondFragment)
+                            .addToBackStack(null).commit();
+
+                    progressBar.setProgress(100);
+                    btnNext.setText("Next");
+                }
+                else if (state == 2) {
+                    AddObject_SetDetails thirdFragment = new AddObject_SetDetails();
+                    //bundle with arguments...
+
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, thirdFragment)
+                            .addToBackStack(null).commit();
+
+                    progressBar.setProgress(200);
+                    btnNext.setText("Done");
+                }
+                break;
+            }
+            case R.id.add_object_next: {
+                btnPrev.setEnabled(true);
+                if (state == 0) {
+                    state = 1;
+                    AddCheckpoint_EditObject secondFragment = new AddCheckpoint_EditObject();
+                    //bundle with arguments...
+
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, secondFragment)
+                            .addToBackStack(null).commit();
+                    progressBar.setProgress(100);
+                }
+                else if (state == 1) {
+                    state = 2;
+                    AddObject_SetDetails thirdFragment = new AddObject_SetDetails();
+                    //bundle with arguments...
+
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.fragment_container, thirdFragment)
+                            .addToBackStack(null).commit();
+
+                    btnNext.setText("Done");
+                    btnNext.setEnabled(false); //for now
+                    progressBar.setProgress(200);
+                }
+            }
+        }
     }
 }
---
+++ WalkHike/app/src/main/java/tamara/mosis/elfak/walkhike/Fragments/AddObject_SetDetails.java
@@ -0,0 +1,26 @@
+package tamara.mosis.elfak.walkhike.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tamara.mosis.elfak.walkhike.R;
+
+public class AddObject_SetDetails extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_object__set_details, container, false);
+    }
+}
